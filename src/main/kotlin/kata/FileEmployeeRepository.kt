3fa1aa4a2adf3d657edb0061cc58b35cf2bcf201
Marco Fracassi <fracassi.marco@gmail.com>
2019-10-08 19:12:27 +0200

    Read record from file

--- src/main/kotlin/kata/FileEmployeeRepository.kt
+++ src/main/kotlin/kata/FileEmployeeRepository.kt
@@ -1,8 +1,20 @@
 package kata
 
-class FileEmployeeRepository : EmployeeRepository{
+import java.io.File
+
+class FileEmployeeRepository(private val file: File) : EmployeeRepository {
 
     override fun bornOn(month: Int, dayOfMonth: Int): List<Employee> {
-        return emptyList()
+        if(!file.exists()) {
+            return emptyList()
+        }
+
+        return file
+            .readLines()
+            .drop(1)
+            .map { it.split(", ") }
+            .filter { it.get(2).split("/").get(1).toInt() == month &&
+                    it.get(2).split("/").get(2).toInt() == dayOfMonth}
+            .map { Employee(it.get(1)) }
     }
 }
--- src/test/kotlin/kata/FileEmployeeRepositoryTest.kt
+++ src/test/kotlin/kata/FileEmployeeRepositoryTest.kt
@@ -1,14 +1,55 @@
 package kata
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class FileEmployeeRepositoryTest {
 
+    private val file = File("/tmp/employees.txt")
+
+    @BeforeEach
+    @AfterEach
+    fun clean() {
+        file.delete()
+    }
+
     @Test
     fun `empty file`() {
-        val employees = FileEmployeeRepository().bornOn(10, 8)
+        val employees = FileEmployeeRepository(file).bornOn(10, 8)
 
         assertThat(employees).isEmpty()
+    }
+
+    @Test
+    fun `file with header`() {
+        file.writeText("last_name, first_name, date_of_birth, email")
+
+        val employees = FileEmployeeRepository(file).bornOn(10, 8)
+
+        assertThat(employees).isEmpty()
+    }
+
+    @Test
+    fun `file with one record`() {
+        file.writeText("last_name, first_name, date_of_birth, email")
+        file.appendText("\nDoe, John, 1982/10/08, john.doe@foobar.com")
+
+        val employees = FileEmployeeRepository(file).bornOn(10, 8)
+
+        assertThat(employees.single()).isEqualTo(Employee("John"))
+    }
+
+    @Test
+    fun `filter by born date`() {
+        file.writeText("last_name, first_name, date_of_birth, email")
+        file.appendText("\nDoe, John, 1982/10/08, john.doe@foobar.com")
+        file.appendText("\nDoe, Alex, 1982/10/09, alex.doe@foobar.com")
+
+        val employees = FileEmployeeRepository(file).bornOn(10, 8)
+
+        assertThat(employees.single()).isEqualTo(Employee("John"))
     }
 }
